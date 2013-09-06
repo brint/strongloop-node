ab17bbe53b272748e0ba8cd55ef4b0791d1b989b
Ryan Walker <ryan.walker@rackspace.com>
2013-09-05 22:46:57 -0500

    Update dependencies.

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'ryan.walker@rackspace.com'
 license          'Apache 2.0'
 description      'Installs/Configures StrongLoop'
 #long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
 
-%w{apt build-essential firewall openssl}.each do |cookbook|
+%w{apt build-essential firewall nginx openssl supervisor}.each do |cookbook|
   depends cookbook
 end
