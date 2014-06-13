720afd2c48e94ff93465f5ce9f58a48a47be9af6
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-06-13 16:02:52 -0500

    Version bump to 0.1.5 (for development)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ryan.walker@rackspace.com'
 license          'Apache 2.0'
 description      'Installs/Configures StrongLoop'
 #long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 %w{apt build-essential firewall nginx nodejs npm openssl supervisor}.each do |cookbook|
   depends cookbook
